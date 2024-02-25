5cbdd56db4b501630704111295d35223f8ce9be7
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-25 12:24:01 +0530

    server setup

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './db/connectDB.js';
+import userRouter from './routes/userRoute.js';
+dotenv.config()
+
+const app = express();
+
+ 
+
+//express middlewares 
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'))
+
+
+
+//routes
+app.use('/api/users' , userRouter)
+
+//DB Connection
+connectDB()
+
+app.listen(process.env.PORT , ()=>{
+    console.log(`Server listening on port ${process.env.PORT}`);
+})
+
+
