7ff4daac031d4e0422dbe247d4f7ff932abf5891
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-27 20:36:14 +0530

    isModified

--- models/userModel.js
+++ models/userModel.js
@@ -38,7 +38,8 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true })
 
 
-userSchema.pre('save' , async function() {
+userSchema.pre('save' , async function(next) {
+    if(!this.isModified("password")) return next();
     this.password = await bcrypt.hash(this.password , 10)
 })
 
