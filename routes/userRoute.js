eabe2cd0adab1480e6bc4011eebcd554d94b5244
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-28 01:10:06 +0530

    update picture route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import { getUserProfile, loginUser, logoutUser, registerUser, updateUserPassword, updateUserProfile } from '../controllers/userController.js';
+import { getUserProfile, loginUser, logoutUser, registerUser, updateProfilePicture, updateUserPassword, updateUserProfile } from '../controllers/userController.js';
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
+import { singleUpload } from '../middlewares/multer.js';
 
 const userRouter = express.Router();
 
@@ -10,5 +11,7 @@ userRouter.get('/profile', isAuthenticated ,getUserProfile)
 userRouter.get('/logout', isAuthenticated ,logoutUser)
 userRouter.put('/update-profile', isAuthenticated , updateUserProfile)
 userRouter.put('/update-password', isAuthenticated , updateUserPassword)
+
+userRouter.put('/update-picture', isAuthenticated ,  singleUpload , updateProfilePicture)
 
 export default userRouter
