9cbd20866ddd19e3d24ab1fdbd4e5e9ddb35a0b7
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-28 01:09:48 +0530

    added public_id/url

--- models/userModel.js
+++ models/userModel.js
@@ -33,7 +33,12 @@ const userSchema = new mongoose.Schema({
         required: [true, "phone no is required"],
     },
     profilePic: {
-       type : String,
+       public_id :{
+        type: String,
+       },
+       url : {
+        type: String,
+       },
     }
 }, { timestamps: true })
 
