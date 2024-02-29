509740a387c08396a05792bca6a7149ae3e28f8f
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-29 19:58:25 +0530

    error handeling middleware

--- middlewares/handleError.js
+++ middlewares/handleError.js
@@ -1,6 +1,7 @@
 
-export const handleErrorMiddleware = (res, statusCode, message, error) => {
-  res.status(statusCode).json({
+// catch block error handler middleware function
+export const handleErrorMiddlewareFunction = (res, statusCode, message, error) => {
+  return res.status(statusCode).json({
     success: false,
     message: message || 'Internal Server Error',
     error: error ? error.message : null
@@ -8,3 +9,20 @@ export const handleErrorMiddleware = (res, statusCode, message, error) => {
 }
 
 
+
+//Not found/validation error middlewareFunction function
+export const handleNotFoundMiddlewareFunction = (res, message) => {
+  return res.status(404).json({
+    success: false,
+    message: message || 'Not Found',
+  });
+};
+
+
+//handle cast error
+export const handleCastErrorMiddlewareFunction = (res, message) => {
+  return res.status(404).json({
+    success: false,
+    message: message || 'Invalid Id'
+  });
+};
