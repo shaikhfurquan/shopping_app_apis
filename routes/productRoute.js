8d78ede16406b96031265c40b60175ef4977e848
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-03-06 13:49:04 +0530

    review and top product

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createProduct, deleteProduct, deleteProductPicture, getAllProducts, getSingleProduct, updateProduct, updateProductPicture } from '../controllers/productController.js';
+import { createProduct, deleteProduct, deleteProductPicture, getAllProducts, getSingleProduct, updateProductReview, updateProduct, updateProductPicture, getTopProducts } from '../controllers/productController.js';
 import { isAdmin, isAuthenticated } from '../middlewares/authMiddleware.js';
 import { singleUpload } from '../middlewares/multer.js';
 
@@ -7,6 +7,7 @@ import { singleUpload } from '../middlewares/multer.js';
 const productRouter = express.Router();
 
 productRouter.get('/get-all' , getAllProducts)
+productRouter.get('/get-top' , getTopProducts)
 productRouter.get('/get/:id' , getSingleProduct)
 productRouter.post('/create' , isAuthenticated , isAdmin,  singleUpload , createProduct)
 productRouter.put('/update/:id' , isAuthenticated  , updateProduct)
@@ -14,5 +15,6 @@ productRouter.put('/update/pic/:id' , isAuthenticated , isAdmin,  singleUpload  
 productRouter.delete('/delete/pic/:id', isAuthenticated , isAdmin,  deleteProductPicture)
 productRouter.delete('/delete/:id', isAuthenticated , isAdmin,  deleteProduct)
 
+productRouter.put('/:id/review' , isAuthenticated , updateProductReview)
 
 export default productRouter
