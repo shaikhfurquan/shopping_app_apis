1c28268283cff8096e2a3b2623c7440638191f1c
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-03-08 00:06:28 +0530

    removed comment

--- helper/generateToken.js
+++ helper/generateToken.js
@@ -2,7 +2,6 @@ import JWT from 'jsonwebtoken'
 
 export const generateToken = async(user) =>{
     try {
-        console.log("user " , user);
         const token =  JWT.sign({_id : user._id} , process.env.JWT_SECRET , {expiresIn : '7d'})
         // console.log(token);
         return token
