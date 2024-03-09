ccf1cf2390f951c4048123236d4ab21d9e025d57
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-03-09 12:19:59 +0530

    rate limit pks

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,22 @@
 import express from 'express'
+import { rateLimit } from 'express-rate-limit'
 import { getUserProfile, loginUser, logoutUser, registerUser, resetUserPassword, updateProfilePicture, updateUserPassword, updateUserProfile } from '../controllers/userController.js';
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
 import { singleUpload } from '../middlewares/multer.js';
 
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    // store: ... , // Redis, Memcached, etc. See below.
+})
+
 const userRouter = express.Router();
 
-userRouter.post('/register', registerUser)
-userRouter.post('/login', loginUser)
+userRouter.post('/register', limiter ,registerUser)
+userRouter.post('/login', limiter ,loginUser)
 userRouter.get('/profile', isAuthenticated ,getUserProfile)
 userRouter.get('/logout', isAuthenticated ,logoutUser)
 userRouter.put('/update-profile', isAuthenticated , updateUserProfile)
